d493193851d0685a0cacae1ab933cae90303228c
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-24 18:41:00 +0100

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: AuthLayout,
+    children: [
+      { path: 'login', component: Login },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  },
+  { path: '**', redirectTo: '/login' }
+];
