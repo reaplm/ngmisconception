8c263ebcb6318c181ead298aa020704e18c64260
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-24 18:42:14 +0100

    import RouterOutlet

--- src/app/layouts/auth-layout/auth-layout.ts
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
+  imports: [RouterOutlet, RouterLinkActive ],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css',
 })
