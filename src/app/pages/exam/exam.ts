ddfee955830d3a693659967e5e4fdc0188865a2c
Pearl Molefe <pdm.molefe@gmail.com>
2026-06-27 18:40:00 +0100

    fetch and display questions

--- src/app/pages/exam/exam.ts
+++ src/app/pages/exam/exam.ts
@@ -1,18 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../auth-service';
 import { Router } from '@angular/router';
+import { QuestionService } from '../../services/question-service';
+import { Question } from '../../models/question';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-exam',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exam.html',
   styleUrl: './exam.css',
 })
-export class Exam {
+export class Exam implements OnInit{
   private authService = inject(AuthService);
+  private questionService = inject(QuestionService)
   private router = inject(Router);
+  private cdr = inject(ChangeDetectorRef);
   
   userEmail: string = '';
+  questions: Question[] = [];
+  isLoading = true;
+  errorMessage = '';
 
   ngOnInit(): void {
     // Check if user is logged in
@@ -24,10 +32,30 @@ export class Exam {
     // Get user email
     const user = this.authService.getUser();
     this.userEmail = user?.email || 'User';
+
+    //Fetch exam questions
+    this.loadQuestions();
   }
 
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  loadQuestions(): void {
+    this.questionService.getQuestions().subscribe({
+      next: (response :any) => {
+        console.error('Response:', response);
+        this.questions = response;
+        this.isLoading = false;
+
+        this.cdr.detectChanges();
+      },
+      error: (error :any) => {
+        console.error('Database fetch error:', error);
+        this.errorMessage = 'Failed to load exam questions. Please try again.';
+        this.isLoading = false;
+      }
+    });
   }
 }
