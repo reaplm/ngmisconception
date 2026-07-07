5ab71816f0195be3ffccbe045b3f63bddb38a306
Pearl Molefe <pdm.molefe@gmail.com>
2026-07-07 12:56:49 +0100

    delete misconceptions page

--- src/app/pages/misconceptions/misconceptions.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-misconceptions',
-  imports: [],
-  templateUrl: './misconceptions.html',
-  styleUrl: './misconceptions.css',
-})
-export class Misconceptions {}
