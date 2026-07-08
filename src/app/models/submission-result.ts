c7a315bc3e4539752fbdf831ce9a3149a104646a
Pearl Molefe <pdm.molefe@gmail.com>
2026-07-08 19:44:02 +0100

    include best answer in submit response

--- src/app/models/submission-result.ts
+++ src/app/models/submission-result.ts
@@ -2,11 +2,14 @@ export interface SubmissionResult {
   detected: boolean;
   highest_score: number;
   threshold: number;
+  question_id: number;
   student_answer: string;
+  question_text: string;
   misconception: {
     id: number;
     text: string;
     question_id: number;
   } | null;
+  best_answer: string | null;
   message: string | null;
 }
